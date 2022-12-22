92f852cea1029e2fb1495422035cf12ff653c03c
darshan-crypto <76841538+darshan-crypto@users.noreply.github.com>
2022-12-22 19:16:28 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,20 @@ class Home extends Component {
 		}
 	}
 
-	handleChange = (e) => this.setState({ url: e.target.value })
+	handleChange = (e) => {this.setState({ url: e.target.value })
+
+
+//   alert("seting state",this.state.url);
+}
 
 	join = () => {
 		if (this.state.url !== "") {
 			var url = this.state.url.split("/")
+
 			window.location.href = `/${url[url.length-1]}`
+
+           // console.log("state is ",this.state.url);
+			//console.log("joining meeting",url[url.length-1]);
 		} else {
 			var url = Math.random().toString(36).substring(2, 7)
 			window.location.href = `/${url}`
@@ -42,9 +50,9 @@ class Home extends Component {
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>&nbsp; Start or join a meeting</p>
+					{/* <Input placeholder="URL" onChange={e => this.handleChange(e)} /> */}
+					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Create Room</Button>
 				</div>
 			</div>
 		)
